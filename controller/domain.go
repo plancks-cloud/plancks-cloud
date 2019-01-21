540d5ac22d0c7807a3477ae34d120843c4e25d09
Just1689 <Just1689@gmail.com>
2019-01-21 21:42:51 +0200

    Controllers

---
+++ controller/domain.go
@@ -0,0 +1,7 @@
+package controller
+
+import "github.com/just1689/plancks-cloud/model"
+
+func GetAllDomains() *[]model.Domain {
+
+}
---
+++ controller/service.go
@@ -0,0 +1,7 @@
+package controller
+
+import "github.com/just1689/plancks-cloud/model"
+
+func GetAllServices() *[]model.Service {
+
+}
