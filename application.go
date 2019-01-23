e340df166d7b2968bec4d2ae1cfbf02341ca7d98
Justin Tamblyn <just1689@gmail.com>
2019-01-23 13:34:01 +0200

    DB init

--- application.go
+++ application.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/plancks-cloud/plancks-cloud/io/http-admin"
+	"github.com/plancks-cloud/plancks-cloud/io/mem"
 	"log"
 )
 
 func main() {
 	log.Println("☁️☁️☁️ Planck's Cloud is starting ☁️☁️☁️")
+
+	mem.Init()
 
 	http_admin.Startup()
 
