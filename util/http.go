cbd5ca979eb3649292e6b37409bd9c13240cff32
Just1689 <Just1689@gmail.com>
2019-01-24 21:44:00 +0200

    Simple responses

--- util/http.go
+++ util/http.go
@@ -1,6 +1,9 @@
 package util
 
 import (
+	"encoding/json"
+	"github.com/valyala/fasthttp"
+	"net/http"
 	"strings"
 )
 
@@ -8,5 +11,21 @@ func HostOfURL(url string) string {
 	//TODO: proper handling with errors
 	sl := strings.Split(url, ":")
 	return sl[0]
+}
 
+func WriteJsonResponseToReq(ctx *fasthttp.RequestCtx, code int, resp []byte) {
+	ctx.Response.SetStatusCode(code)
+	ctx.Response.SetBody(resp)
+}
+
+func WriteErrorToReq(ctx *fasthttp.RequestCtx, msg string) {
+	ctx.Response.SetStatusCode(http.StatusInternalServerError)
+	var r struct {
+		OK  bool   `json:"ok"`
+		Msg string `json:"msg"`
+	}
+	r.OK = false
+	r.Msg = msg
+	b, _ := json.Marshal(&r)
+	ctx.Response.SetBody(b)
 }
