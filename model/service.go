f818f39a827b79993275b01c7b238921e4873138
Kevin Stroud <g33k3n01d@gmail.com>
2019-01-25 13:11:43 +0200

    Add attributes to service

--- model/service.go
+++ model/service.go
@@ -3,7 +3,9 @@ package model
 const ServiceCollectionName = "Service"
 
 type Service struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Image string `json:"image"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Image       string `json:"image"`
+	Replicas    int    `json:"replicas,omitempty"`
+	MemoryLimit int    `json:"memoryLimit,omitempty"`
 }
