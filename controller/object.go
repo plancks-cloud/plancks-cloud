491b85c56d5e447697efdc754905f9d0041fd6eb
Justin Tamblyn <just1689@gmail.com>
2019-01-25 10:00:22 +0200

    Refactor - reduce complexity

--- controller/object.go
+++ controller/object.go
@@ -9,32 +9,42 @@ import (
 
 func HandleApply(item *model.Object) (err error) {
 	if item.Type == "route" {
-		var routes = &[]model.Route{}
-		err = json.Unmarshal(item.List, routes)
-		if err != nil {
-			return
-		}
-		err = InsertManyRoutes(routes)
-		if err != nil {
-			return
-		}
-		RefreshProxy()
-
+		return handleApplyRoutes(item.List)
 	} else if item.Type == "service" {
-		var s = &[]model.Service{}
-		err = json.Unmarshal(item.List, s)
-		if err != nil {
-			return
-		}
-		err = InsertManyServices(s)
-		if err != nil {
-			return
-		}
+		err = handleApplyServices(item.List)
 
 		//TODO: Call docker client ensure services are there and up to day
 
 	} else {
 		err = errors.New(fmt.Sprint("Unknown type for /apply object: ", item.Type))
+	}
+	return
+}
+
+func handleApplyRoutes(list json.RawMessage) (err error) {
+	var routes = &[]model.Route{}
+	err = json.Unmarshal(list, routes)
+	if err != nil {
+		return
+	}
+	err = InsertManyRoutes(routes)
+	if err != nil {
+		return
+	}
+	RefreshProxy()
+	return
+
+}
+
+func handleApplyServices(list json.RawMessage) (err error) {
+	var s = &[]model.Service{}
+	err = json.Unmarshal(list, s)
+	if err != nil {
+		return
+	}
+	err = InsertManyServices(s)
+	if err != nil {
+		return
 	}
 	return
 }
