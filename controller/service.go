da4cca977f9a176da86be87bbf2e0093b0a46b6f
Just1689 <Just1689@gmail.com>
2019-01-30 16:51:15 +0200

    Delete services

--- controller/service.go
+++ controller/service.go
@@ -4,6 +4,8 @@ import (
 	"github.com/hashicorp/go-memdb"
 	"github.com/plancks-cloud/plancks-cloud/io/mem"
 	"github.com/plancks-cloud/plancks-cloud/model"
+	"github.com/plancks-cloud/plancks-docker/controller/pc-docker"
+	pc_model "github.com/plancks-cloud/plancks-docker/model"
 	"github.com/sirupsen/logrus"
 )
 
@@ -41,10 +43,25 @@ func InsertManyServices(l *[]model.Service) (err error) {
 func DeleteManyServices(l *[]model.Service) (err error) {
 	for _, item := range *l {
 		_, err = mem.Delete(model.ServiceCollectionName, model.ServiceCollectionID, item.ID)
+		pc_docker.DeleteServices(convertServices(l))
 		if err != nil {
 			logrus.Error(err)
 			return err
 		}
+	}
+	return
+}
+
+func convertServices(services *[]model.Service) (res []pc_model.ServiceState) {
+	res = make([]pc_model.ServiceState, len(*services))
+	for _, service := range *services {
+		item := pc_model.ServiceState{
+			ID:          service.ID,
+			Name:        service.Name,
+			MemoryLimit: service.MemoryLimit,
+			Image:       service.Image,
+		}
+		res = append(res, item)
 	}
 	return
 }
