267df0e1d38e8dfb1ff66fda06822f6355f21568
Just1689 <Just1689@gmail.com>
2019-02-12 00:02:32 +0200

    Routes can support SSL

--- model/route.go
+++ model/route.go
@@ -9,6 +9,12 @@ type Route struct {
 	ID         string `json:"id"`
 	DomainName string `json:"domainName"`
 	Address    string `json:"address"`
+	SSL        SSL    `json:"ssl"`
+}
+
+type SSL struct {
+	Email  string `json:"email"`
+	Accept bool   `json:"accept"`
 }
 
 func (r *Route) GetHttpAddress() string {
