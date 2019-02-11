7e45bcf90c09c39ee7cda90ab94f6d5528f0b36e
Just1689 <Just1689@gmail.com>
2019-02-12 00:28:35 +0200

    Check routes insertMany

--- controller/route_test.go
+++ controller/route_test.go
@@ -27,3 +27,61 @@ func TestGetAllRoutesCopy(t *testing.T) {
 	}
 
 }
+
+func TestGetAllRoutesCopyTwoItems(t *testing.T) {
+	mem.Init()
+	r := model.Route{ID: "1", DomainName: "test.com", Address: "1"}
+	err := Upsert(&r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	r = model.Route{ID: "2", DomainName: "test2.com", Address: "2"}
+	err = Upsert(&r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	arr := GetAllRoutesCopy()
+	if len(arr) != 2 {
+		t.Error("Could not find inserted route")
+
+	}
+
+	ok := reflect.DeepEqual(arr[0], r)
+	if !ok {
+		t.Error("Stored route not the same as found route")
+	}
+
+}
+
+func TestGetAllRoutesCopyTwoItemsBatch(t *testing.T) {
+	mem.Init()
+	var arr []model.Route
+	r1 := model.Route{ID: "1", DomainName: "test.com", Address: "1"}
+	arr = append(arr, r1)
+
+	r2 := model.Route{ID: "2", DomainName: "test2.com", Address: "2"}
+	arr = append(arr, r2)
+
+	err := InsertManyRoutes(&arr)
+	if err != nil {
+		t.Error(err)
+	}
+
+	res := GetAllRoutesCopy()
+	if len(res) != 2 {
+		t.Error("Could not find inserted route")
+
+	}
+
+	ok := reflect.DeepEqual(arr[0], r1)
+	if !ok {
+		t.Error("Stored route not the same as found route")
+	}
+	ok = reflect.DeepEqual(arr[1], r2)
+	if !ok {
+		t.Error("Stored route not the same as found route")
+	}
+
+}
