a78a3abd2c9b40057f90cd817c6977a9f6653e6d
Just1689 <Just1689@gmail.com>
2019-02-13 22:14:05 +0200

    Controller for config

---
+++ controller/config.go
@@ -0,0 +1,57 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/hashicorp/go-memdb"
+	"github.com/plancks-cloud/plancks-cloud/io/mem"
+	"github.com/plancks-cloud/plancks-cloud/model"
+	"github.com/sirupsen/logrus"
+)
+
+func SaveConfig(item *model.Config) error {
+	return mem.Push(item)
+}
+
+func GetPersistPath() string {
+	return fmt.Sprint(GetConfig(model.PersistPath), "/config")
+}
+
+func GetConfig(id string) (r model.Config) {
+	resp := make(chan model.Config)
+	go func() {
+		ite, err := mem.GetAll(model.ConfigCollectionName)
+		iteratorToManyConfigs(ite, err, resp)
+		close(resp)
+	}()
+	for i := range resp {
+		if i.ID == id {
+			t := i
+			r = t
+			return
+		}
+	}
+	return
+}
+
+func iteratorToManyConfigs(iterator memdb.ResultIterator, err error, out chan model.Config) {
+	if err != nil {
+		logrus.Error(err.Error())
+		return
+	}
+	if iterator == nil {
+		return
+	}
+	more := true
+	count := 0
+	for more {
+		next := iterator.Next()
+		if next == nil {
+			more = false
+			continue
+		}
+		item := next.(*model.Config)
+		out <- *item
+		count++
+	}
+	logrus.Debugln(fmt.Sprintf("Route iterator counts: %d", count))
+}
