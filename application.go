088ee646fb4e592ec0b8aa54060273ee69ebe29b
Just1689 <Just1689@gmail.com>
2019-02-13 22:14:21 +0200

    Sync is about files in persist path now

--- application.go
+++ application.go
@@ -5,27 +5,23 @@ import (
 	"github.com/plancks-cloud/plancks-cloud/controller"
 	"github.com/plancks-cloud/plancks-cloud/io/http-admin"
 	"github.com/plancks-cloud/plancks-cloud/io/mem"
-	"github.com/plancks-cloud/plancks-cloud/model"
 	"github.com/sirupsen/logrus"
 )
 
 var (
-	addr       = flag.String("admin", ":6227", "TCP address to listen to")
-	persistID  = flag.String("persistID", "", "Persistence ID")
-	persistKey = flag.String("persistKey", "", "Persistence key")
-	persistUrl = flag.String("persistURL", "", "Persistence URL")
+	addr        = flag.String("admin", ":6227", "TCP address to listen to")
+	persistPath = flag.String("persistPath", "", "Persistence path")
 )
 
 func main() {
 	flag.Parse()
 	logrus.Println("☁️☁️☁️ Planck's Cloud is starting ☁️☁️☁️")
 
-	cred := getCredentials()
-	logrus.Println("...️ pulling down state")
-	controller.StartupSync(cred)
-
 	logrus.Println("...️ starting in-memory DB")
 	mem.Init()
+
+	logrus.Println("...️ pulling down state")
+	controller.StartupSync(persistPath)
 
 	logrus.Println("... ️starting health server")
 	controller.StartHealthServer()
@@ -33,12 +29,4 @@ func main() {
 	logrus.Println("...️ starting api")
 	http_admin.Startup(addr)
 
-}
-
-func getCredentials() *model.Cred {
-	return &model.Cred{
-		URL: persistUrl,
-		ID:  persistID,
-		Key: persistKey,
-	}
 }
--- controller/sync.go
+++ controller/sync.go
@@ -2,37 +2,51 @@ package controller
 
 import (
 	"encoding/json"
-	"github.com/plancks-cloud/plancks-cloud/io/gcp"
+	"fmt"
 	"github.com/plancks-cloud/plancks-cloud/model"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
 )
 
-func StartupSync(cred *model.Cred) {
-	if len(*cred.ID) == 0 || len(*cred.URL) == 0 || len(*cred.Key) == 0 {
-		logrus.Info("Not enough parameters to sync to and from cloud. Ignoring.")
+func StartupSync(persistPath *string) {
+	if len(*persistPath) == 0 {
+		logrus.Info("Persist path not provided. Not starting persist")
 		return
 	}
 
-	syncServicesDown(cred)
-	syncRoutesDown(cred)
+	err := SaveConfig(&model.Config{ID: model.PersistPath, Val: *persistPath})
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+
+	configPath := GetPersistPath()
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		err = os.Mkdir(configPath, os.ModeDir)
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+	}
+
+	//syncServicesDown()
+	syncRoutesDown()
 }
 
-func syncRoutesDown(cred *model.Cred) {
-	r, err := gcp.GetCollection(*cred.URL, *cred.ID, *cred.Key, model.RouteCollectionName)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
+func syncRoutesDown() {
 
-	var sl *[]model.Route
-	err = json.Unmarshal(r, sl)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-
-	if len(*sl) > 0 {
-		err = InsertManyRoutes(sl)
+	configPath := GetPersistPath()
+	file := fmt.Sprint(configPath, "/", model.RouteCollectionName, ".json")
+	if _, err := os.Stat(file); err == nil {
+		b, err := ioutil.ReadFile(file)
+		var arr []model.Route
+		err = json.Unmarshal(b, &arr)
+		if err != nil {
+			logrus.Error(err)
+			return
+		}
+		err = InsertManyRoutes(&arr)
 		if err != nil {
 			logrus.Error(err)
 			return
@@ -41,60 +55,60 @@ func syncRoutesDown(cred *model.Cred) {
 
 }
 
-func syncRoutesUp(endpoint, id, key string) {
-	sl := GetAllRoutesCopy()
-	b, err := json.Marshal(sl)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-	r, err := gcp.SetCollection(endpoint, id, key, model.RouteCollectionName, b)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-	msg := string(r)
-	logrus.Println(msg)
-
-}
-
-func syncServicesDown(cred *model.Cred) {
-	r, err := gcp.GetCollection(*cred.URL, *cred.ID, *cred.Key, model.ServiceCollectionName)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-
-	var sl *[]model.Service
-	err = json.Unmarshal(r, sl)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-
-	if len(*sl) > 0 {
-		err = InsertManyServices(sl)
-		if err != nil {
-			logrus.Error(err)
-			return
-		}
-	}
-
-}
-
-func syncServicesUp(endpoint, id, key string) {
-	sl := GetAllRoutesCopy()
-	b, err := json.Marshal(sl)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-	r, err := gcp.SetCollection(endpoint, id, key, model.RouteCollectionName, b)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-	msg := string(r)
-	logrus.Println(msg)
-
-}
+//func syncRoutesUp() {
+//	sl := GetAllRoutesCopy()
+//	b, err := json.Marshal(sl)
+//	if err != nil {
+//		logrus.Error(err)
+//		return
+//	}
+//	r, err := gcp.SetCollection(endpoint, id, key, model.RouteCollectionName, b)
+//	if err != nil {
+//		logrus.Error(err)
+//		return
+//	}
+//	msg := string(r)
+//	logrus.Println(msg)
+//
+//}
+//
+//func syncServicesDown() {
+//	r, err := gcp.GetCollection(*cred.URL, *cred.ID, *cred.Key, model.ServiceCollectionName)
+//	if err != nil {
+//		logrus.Error(err)
+//		return
+//	}
+//
+//	var sl *[]model.Service
+//	err = json.Unmarshal(r, sl)
+//	if err != nil {
+//		logrus.Error(err)
+//		return
+//	}
+//
+//	if len(*sl) > 0 {
+//		err = InsertManyServices(sl)
+//		if err != nil {
+//			logrus.Error(err)
+//			return
+//		}
+//	}
+//
+//}
+//
+//func syncServicesUp() {
+//	sl := GetAllRoutesCopy()
+//	b, err := json.Marshal(sl)
+//	if err != nil {
+//		logrus.Error(err)
+//		return
+//	}
+//	r, err := gcp.SetCollection(endpoint, id, key, model.RouteCollectionName, b)
+//	if err != nil {
+//		logrus.Error(err)
+//		return
+//	}
+//	msg := string(r)
+//	logrus.Println(msg)
+//
+//}
